ab4b5667f3fd739acf8906ec330f215be94a5d71
Chris F <CF-public@gmx.net>
2022-11-10 00:41:27 +0100

    Reverted to using ReferenceAssemblies since the Publicized library threw an access exception, will just stick with reflection

--- Leveling/PatchGetSkillLevelChange.cs
+++ Leveling/PatchGetSkillLevelChange.cs
@@ -12,8 +12,7 @@ internal class PatchGetSkillLevelChange
 {
 	public static bool Prefix(ref DefaultCharacterDevelopmentModel __instance, ref int __result, Hero hero, SkillObject skill, float skillXp)
 	{
-		//int[] array = (int[])typeof(DefaultCharacterDevelopmentModel).GetField("_xpRequiredForSkillLevel", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
-		var array = __instance._xpRequiredForSkillLevel;
+		int[] array = (int[])typeof(DefaultCharacterDevelopmentModel).GetField("_xpRequiredForSkillLevel", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
 		int start = hero.GetSkillValue(skill);
 		int next = start;
 		while (next < 300)
--- Leveling/PatchSetInitialLevelFromSkills.cs
+++ Leveling/PatchSetInitialLevelFromSkills.cs
@@ -23,8 +23,7 @@ internal class PatchSetInitialLevelFromSkills
 			var xp = PatchXpRequiredForSkillLevel.XpRequiredForSkillLevel[skillValue];
 			totalXP += xp;
 		}
-		__instance.TotalXp = totalXP;
-		//typeof(HeroDeveloper).GetProperty(nameof(HeroDeveloper.TotalXp)).GetSetMethod(true).Invoke(__instance, new object[] { totalXP });
+		typeof(HeroDeveloper).GetProperty(nameof(HeroDeveloper.TotalXp)).GetSetMethod(true).Invoke(__instance, new object[] { totalXP });
 		return false;
 	}
 }
