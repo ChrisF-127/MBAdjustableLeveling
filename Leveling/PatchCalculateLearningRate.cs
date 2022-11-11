2417a43d7e78c9491f65254b40da3aa271e2ad49
Chris F <CF-public@gmx.net>
2022-11-11 20:12:17 +0100

    Reverted to using vanilla skill xp curve // changed skill xp modifier: focus points give a static learning rate increase, besides increasing the learning limit, the learning rate bonus from attribute points reduces towards the learning limit

--- Leveling/PatchCalculateLearningRate.cs
+++ Leveling/PatchCalculateLearningRate.cs
@@ -18,24 +18,37 @@ namespace AdjustableLeveling.Leveling;
 })]
 internal class PatchCalculateLearningRate
 {
-	private static TextObject _attributeText = new TextObject("{=AT6v10NK}Attribute");
-
-	private static TextObject _skillFocusText = new TextObject("{=MRktqZwu}Skill Focus");
-
-	private static TextObject _overLimitText = new TextObject("{=bcA7ZuyO}Learning Limit Exceeded");
+	private static readonly TextObject _skillFocusText = new TextObject("{=MRktqZwu}Skill Focus");
+	private static readonly TextObject _overLimitText = new TextObject("{=bcA7ZuyO}Learning Limit Exceeded");
 
 	public static bool Prefix(ref ExplainedNumber __result, ref DefaultCharacterDevelopmentModel __instance, int attributeValue, int focusValue, int skillValue, int characterLevel, TextObject attributeName, bool includeDescriptions = false)
 	{
-		var explainedNumber = new ExplainedNumber(0.5f, includeDescriptions: true);
-		explainedNumber.AddFactor(1f * attributeValue - 1f, attributeName);
-		explainedNumber.AddFactor(focusValue * 2f, _skillFocusText);
-		int num = MathF.Round(__instance.CalculateLearningLimit(attributeValue, focusValue, null).ResultNumber);
-		if (skillValue >= num)
-		{
-			explainedNumber.AddFactor(0f - 1f * attributeValue - focusValue * 2f, _overLimitText);
-		}
+		int limit = MathF.Round(__instance.CalculateLearningLimit(attributeValue, focusValue, null).ResultNumber);
+		var explainedNumber = new ExplainedNumber(1f, includeDescriptions: true);
+
+		var attributeFactor = (attributeValue - 1f) * (1f - skillValue / (float)limit);
+		var focusFactor = focusValue - 0.5f;
+
+		explainedNumber.AddFactor(attributeFactor, attributeName);
+		explainedNumber.AddFactor(focusFactor, _skillFocusText);
+		
+		if (skillValue >= limit) // set learning rate to 0
+			explainedNumber.AddFactor(-(1f + attributeFactor + focusFactor), _overLimitText);
+
 		explainedNumber.LimitMin(0f);
 		__result = explainedNumber;
 		return false;
+
+		//var explainedNumber = new ExplainedNumber(0.5f, includeDescriptions: true);
+		//explainedNumber.AddFactor(1f * attributeValue - 1f, attributeName);
+		//explainedNumber.AddFactor(focusValue * 2f, _skillFocusText);
+		//int num = MathF.Round(__instance.CalculateLearningLimit(attributeValue, focusValue, null).ResultNumber);
+		//if (skillValue >= num)
+		//{
+		//	explainedNumber.AddFactor(0f - 1f * attributeValue - focusValue * 2f, _overLimitText);
+		//}
+		//explainedNumber.LimitMin(0f);
+		//__result = explainedNumber;
+		//return false;
 	}
 }
